4cc499e84678e26e376f0745c227c9d73f9e7462
Patrick Coxall <mr.coxall@mths.ca>
2022-05-05 22:46:21 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    mySprite.sayText(":)")
+    currentLevel += 1
+    mySprite.sayText(currentLevel)
+    nextScene()
+})
+function nextScene () {
+    tiles.setCurrentTilemap(gameLevels[currentLevel])
+}
+let mySprite: Sprite = null
+let currentLevel = 0
+let gameLevels: tiles.TileMapData[] = []
+gameLevels = [tilemap`level1`, tilemap`level3`]
+currentLevel = 0
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . b b b b b 5 5 5 5 5 5 5 b . . 
+    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+    . . b d 5 5 b 1 f f 5 4 4 c . . 
+    b b d b 5 5 5 d f b 4 4 4 4 4 b 
+    b d d c d 5 5 b 5 4 4 4 4 4 b . 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
+nextScene()
